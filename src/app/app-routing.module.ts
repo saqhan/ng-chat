2f4b79eda93af8ea19c5c3e1bf443119045fa20a
Saqhan <saqhan1@mail.ru>
2020-06-17 22:33:09 +0300

    create new components for desktop & mobile response

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CompComponent} from './comp/comp.component';
+import {MobileComponent} from './mobile/mobile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CompComponent,
+  },
+  {
+    path: 'mobile',
+    component: MobileComponent,
+  },
+  {
+    path: '**',
+    component: CompComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompComponent } from './comp/comp.component';
+import { MobileComponent } from './mobile/mobile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompComponent,
+    MobileComponent
   ],
   imports: [
     BrowserModule,
